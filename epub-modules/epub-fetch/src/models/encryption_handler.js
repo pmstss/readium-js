0002903213363a071499233235122d45844d8149
Juan Corona <juanc@evidentpoint.com>
2014-10-31 10:11:54 -0700

    Fix merge issue.

--- epub-modules/epub-fetch/src/models/encryption_handler.js
+++ epub-modules/epub-fetch/src/models/encryption_handler.js
@@ -11,7 +11,7 @@
 //  used to endorse or promote products derived from this software without specific 
 //  prior written permission.
 
-define(['crypto'], function (crypto) {
+define(['require', 'module'], function (require, module) {
 
     var EncryptionHandler = function (encryptionData) {
         var self = this;
