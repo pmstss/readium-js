d91b7380759fdc4c51ec15a9512e4d3579f924aa
Juan Corona <juanc@evidentpoint.com>
2015-05-19 11:11:51 -0500

    Small refactoring of test spec

--- spec/suites/cfi_nav_logic.spec.js
+++ spec/suites/cfi_nav_logic.spec.js
@@ -2,19 +2,19 @@ describe("A suite", function() {
     beforeEach(function () {
 
         // Generate CFI AST to reference a paragraph in the Moby Dick test features
-        CFI = "epubcfi(/6/14!/4/2/14/1:4)";
-        CFIAST = EPUBcfi.Parser.parse(CFI);
+        this.CFI = "epubcfi(/6/14!/4/2/14/1:4)";
+        this.CFIAST = EPUBcfi.Parser.parse(this.CFI);
 
         var domParser = new window.DOMParser();
 
         // Set up content document
         var contentDocXHTML = jasmine.getFixtures().read("moby_dick_doc.xhtml");
-        contentDocument = domParser.parseFromString(contentDocXHTML, 'text/xml');
-        $contentDocument = $(contentDocument);
+        this.contentDocument = domParser.parseFromString(contentDocXHTML, 'text/xml');
+        this.$contentDocument = $(this.contentDocument);
 
     });
     it("contains spec with an expectation", function() {
-        expect(contentDocument).toBeDefined();
+        expect(this.contentDocument).toBeDefined();
         debugger
     });
 });
