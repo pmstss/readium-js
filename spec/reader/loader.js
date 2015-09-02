87a490dd7a21a2cbdbaaf4daf0b3d30194fab9b4
Juan Corona <juanc@evidentpoint.com>
2015-09-02 12:57:30 -0700

    Make the spec reader bootstrapping work with new requirejs restructuring

--- spec/reader/loader.js
+++ spec/reader/loader.js
@@ -1,90 +1,63 @@
-requirejs.config({
-    "baseUrl": "../",
-    "paths": {
-        //doesn't need to depend on specific versions of these
-        underscore: "vendor/underscore",
-        jquery: "vendor/jquery",
-        backbone: "vendor/backbone",
+require(["readium_shared_js/globalsSetup"], function() {
+    require(['jquery', 'underscore', 'readium_js/Readium'], function($, _, Readium) {
 
-        URIjs: 'vendor/URIjs/URI',
-        punycode: 'vendor/URIjs/punycode',
-        SecondLevelDomains: 'vendor/URIjs/SecondLevelDomains',
-        IPv6: 'vendor/URIjs/IPv6',
-        Readium: '../out/Readium'
-    },
-    "shim": {
-        underscore: {
-            exports: '_'
-        },
-        backbone: {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        Readium: {
-            deps: ['backbone'],
-            exports: 'Readium'
-        }
-    }
-});
+        $(document).ready(function() {
 
-require(['Readium'], function (Readium) {
+            var readerOptions = {
+                //baseUrl: "epub://",
+                el: '#readium-container', //selector for readium container element
+                annotationCSSUrl: new URI(window.location.href).normalize().filename("annotations.css").toString() //absoulte path since it's going to lose the base url inside an iframe
+            };
 
-    $(document).ready(function () {
+            var epubPath = '../epubs/accessible_epub_3';
 
-        var readerOptions = {
-            //baseUrl: "epub://",
-            el: '#readium-container', //selector for readium container element
-            annotationCSSUrl: new URI(window.location.href).normalize().filename("annotations.css").toString() //absoulte path since it's going to lose the base url inside an iframe
-        };
+            var readiumOptions = {
+                jsLibRoot: '../vendor/',
+            };
 
-        var epubPath = '../epubs/accessible_epub_3';
+            var readium = new Readium(readiumOptions, readerOptions);
+            var reader = readium.reader;
 
-        var readiumOptions = {
-            jsLibRoot: '../vendor/',
-        };
+            window.testReader = {
+                readium: readium,
+                reader: reader,
+                waitForFinalPagination: function(cb) {
+                    var finalPaginationChanged = _.debounce(
+                        _.once(function() {
+                            _.defer(function() {
+                                cb.apply(this, arguments);
+                                reader.off(ReadiumSDK.Events.PAGINATION_CHANGED, finalPaginationChanged);
+                            });
+                        }), 300);
+                    reader.on(ReadiumSDK.Events.PAGINATION_CHANGED, finalPaginationChanged);
+                }
+            };
 
-        var readium = new Readium(readiumOptions, readerOptions);
-        var reader = readium.reader;
+            window.parent.postMessage('testReaderReady', '*');
 
-        window.testReader = {
-            readium: readium,
-            reader: reader,
-            waitForFinalPagination: function(cb) {
-                var finalPaginationChanged = _.debounce(
-                    _.once(function () {
-                        _.defer(function () {
-                            cb.apply(this, arguments);
-                            reader.off(ReadiumSDK.Events.PAGINATION_CHANGED, finalPaginationChanged);
-                        });
-                    }), 300);
-                reader.on(ReadiumSDK.Events.PAGINATION_CHANGED, finalPaginationChanged);
-            }
-        };
+            //Buttons for page nav
 
-        window.parent.postMessage('testReaderReady', '*');
+            $('#left').on('click', function() {
+                reader.openPageLeft();
+            });
 
-        //Buttons for page nav
 
-        $('#left').on('click', function () {
-            reader.openPageLeft();
+            $('#right').on('click', function() {
+                reader.openPageRight();
+            });
+
+            $('#bookmark').on('click', function() {
+                prompt('', reader.bookmarkCurrentPage());
+            });
+
+            $('#pageinfo').on('click', function() {
+                prompt('', JSON.stringify(reader.getPaginationInfo().openPages));
+            });
+
+            $('#reflowinfo').on('click', function() {
+                prompt('', JSON.stringify(reader.getPaginationInfo().reflowablePagination));
+            });
+
         });
-
-
-        $('#right').on('click', function () {
-            reader.openPageRight();
-        });
-
-        $('#bookmark').on('click', function () {
-            prompt('', reader.bookmarkCurrentPage());
-        });
-
-        $('#pageinfo').on('click', function () {
-            prompt('', JSON.stringify(reader.getPaginationInfo().openPages));
-        });
-
-        $('#reflowinfo').on('click', function () {
-            prompt('', JSON.stringify(reader.getPaginationInfo().reflowablePagination));
-        });
-
     });
 });
