d16faae1f12b0bd411d21e740a3e91a9b3508355
Viachaslau Tyshkavets <pmstss@gmail.com>
2016-03-24 18:48:29 +0300

    iframe_zip_loader fixes after merge

--- js/epub-fetch/iframe_zip_loader.js
+++ js/epub-fetch/iframe_zip_loader.js
@@ -183,43 +183,45 @@ function ($, URI, Globals, Helpers, IFrameLoader, _, ContentTypeDiscovery) {
                         });
                 }
             }
-            
-            $('svg', doc).load(function(){
-                console.log('SVG loaded');
-            });
-                
+
             self.updateIframeEvents(iframe);
-                
-            var callback = basicIframeLoader._onIframeLoad.call(bind, iframe, callbackOrig);
-                
+
+            var callback = basicIframeLoader._onIframeLoad.bind(basicIframeLoader, iframe, callbackOrig);
             var mathJax = iframe.contentWindow.MathJax;
             if (mathJax) {
                 console.log("MathJax VERSION: " + mathJax.cdnVersion + " // " + mathJax.fileversion + " // " + mathJax.version);
-                
+
                 var useFontCache = true; // default in MathJax
-                
+
                 // Firefox fails to render SVG otherwise
                 if (mathJax.Hub.Browser.isFirefox) {
                     useFontCache = false;
                 }
-                
+
                 // Chrome 49+ fails to render SVG otherwise
                 // https://github.com/readium/readium-js/issues/138
                 if (mathJax.Hub.Browser.isChrome) {
                     useFontCache = false;
                 }
-                
+
                 // Edge fails to render SVG otherwise
                 // https://github.com/readium/readium-js-viewer/issues/394#issuecomment-185382196
                 if (window.navigator.userAgent.indexOf("Edge") > 0) {
                     useFontCache = false;
                 }
-                
-                mathJax.Hub.Config({showMathMenu:false, messageStyle: "none", showProcessingMessages: true, SVG:{useFontCache:useFontCache}});
-            
+
+                mathJax.Hub.Config({
+                    showMathMenu: false,
+                    messageStyle: "none",
+                    showProcessingMessages: true,
+                    SVG: {
+                        useFontCache: useFontCache
+                    }
+                });
+
                 // If MathJax is being used, delay the callback until it has completed rendering
                 var mathJaxCallback = _.once(callback);
-                
+
                 try {
                     mathJax.Hub.Queue(mathJaxCallback);
                 } catch (err) {
